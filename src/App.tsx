b84d1b0d5759d9ccdc23f592aa6d801d10f62772
Pek, Jonetta <jpek009@e.ntu.edu.sg>
2024-05-05 17:18:16 +0800

    feat: add routing to tracking id search bar, login, create an order pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import { NavBar } from "./components/nav-bar/NavBar"
+import TrackSearchBarPage from "./pages/TrackSearchBarPage"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { LoginPage } from "./pages/LoginPage"
+import { ShippingFormPage } from "./pages/ShippingFormPage"
+import { RegisterPage } from "./pages/RegisterPage"
 
 function App() {
   return (
-    <>
-      <NavBar />
-    </>
+    <Router>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <Routes>
+          <Route index element={<TrackSearchBarPage />} />
+          <Route path="create-a-shipment" element={<ShippingFormPage />} />
+          <Route path="track-a-package" element={<TrackSearchBarPage />} />
+          <Route path="open-an-account" element={<RegisterPage />} />
+          <Route path="login" element={<LoginPage />} />
+        </Routes>
+      </main>
+    </Router>
   )
 }
 
---
+++ src/components/register/RegisterForm.tsx
@@ -0,0 +1,8 @@
+
+
+export function RegisterForm() {
+    return (
+        <>
+        </>
+    )
+}
