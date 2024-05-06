c53b9cb5dc75ad7ba7cbed91819e55ccf293325b
Pek, Jonetta <jpek009@e.ntu.edu.sg>
2024-05-06 11:28:11 +0800

    feat: add service type card selection for shipping service subsection

--- src/components/order-form/DeliveryServiceCard.tsx
+++ src/components/order-form/DeliveryServiceCard.tsx
@@ -1,18 +1,30 @@
+import { DeliveryServiceType } from "../../utilities/enums/DeliveryServiceType";
+import "../../Card.css"
+
 type DeliveryServiceCardData = {
     title: string,
     estimatedTimeArrival: string,
     deliveryCharge: string
 }
 
-export function DeliveryServiceCard({ title, estimatedTimeArrival, deliveryCharge}: DeliveryServiceCardData) {
+type DeliveryServiceCardProps = DeliveryServiceCardData & {
+    onSelect: (title: string) => void,
+    selected: boolean
+}
+
+export function DeliveryServiceCard({ title, estimatedTimeArrival, deliveryCharge, onSelect, selected}: DeliveryServiceCardProps) {
+    const handleClick = () => {
+        onSelect(title.toLowerCase() === DeliveryServiceType.Normal ? DeliveryServiceType.Normal : DeliveryServiceType.Express);
+    };
     return (
-        <>
-            <div className="relative flex flex-col border-2 rounded-md h-60 w-60 bg-slate-200 border-slate-200 p-4">
-                <span className="text-xl font-semibold">{title}</span>
-                <span className="text-slate-500">{estimatedTimeArrival}</span>
-                <span className="text-slate-500">by End of Day</span>
-                <span className="absolute bottom-0 left-0 p-4 text-slate-800">${deliveryCharge}</span>
-            </div>
-        </>
+        <div 
+            className={`card ${selected ? 'selected': ''} relative flex flex-col rounded-md h-60 w-60 bg-slate-200 p-4 hover:bg-slate-600 hover:text-white`}
+            onClick={handleClick}
+        >
+            <span className={`hover-inherit text-xl font-semibold ${selected ? 'text-white' : 'text-slate-700'}`}>{title}</span>
+            <span className={`hover-inherit ${selected ? 'text-white' : 'text-slate-500'}`}>{estimatedTimeArrival}</span>
+            <span className={`hover-inherit ${selected ? 'text-white' : 'text-slate-500'}`}>by End of Day</span>
+            <span className={`hover-inherit absolute bottom-0 left-0 p-4 ${selected ? 'text-white' : 'text-slate-700'}`}>${deliveryCharge}</span>
+        </div>
     )
 }
