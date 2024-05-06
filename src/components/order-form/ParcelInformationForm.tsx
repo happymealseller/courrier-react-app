99eef4bdc32a6e9388752781dba95e2d5069b283
Pek, Jonetta <jpek009@e.ntu.edu.sg>
2024-05-06 15:29:48 +0800

    feat: add payment subsection for shipping order form

--- src/components/order-form/ParcelInformationForm.tsx
+++ src/components/order-form/ParcelInformationForm.tsx
@@ -1,3 +1,4 @@
+import { MeasurementUnit } from "../../utilities/enums/MeasurementUnit";
 import { ParcelType } from "../../utilities/enums/ParcelType";
 import { ParcelInformationFormProps } from "../../utilities/type-aliases/order-form/ParcelInformationFormProps";
 import { FormWrapper } from "./FormWrapper";
@@ -22,7 +23,7 @@ export function ParcelInformationForm({ length, width, height, weight, updateFie
                     onChange={e => updateFields({ length: e.target.value })}
                     className="border-2 px-2 rounded-md bg-gray-200 w-5/6" 
                 />
-                <span className="px-2 text-gray-500">cm</span>
+                <span className="px-2 text-gray-500">{MeasurementUnit.centimetre}</span>
             </div>
             <label className="font-bold">Width</label>
             <div>
@@ -34,7 +35,7 @@ export function ParcelInformationForm({ length, width, height, weight, updateFie
                     onChange={e => updateFields({ width: e.target.value })}
                     className="border-2 px-2 rounded-md bg-gray-200 w-5/6" 
                 />
-                <span className="px-2 text-gray-500">cm</span>
+                <span className="px-2 text-gray-500">{MeasurementUnit.centimetre}</span>
             </div>
             <label className="font-bold">Height</label>
             <div>
@@ -46,7 +47,7 @@ export function ParcelInformationForm({ length, width, height, weight, updateFie
                     onChange={e => updateFields({ height: e.target.value })}
                     className="border-2 px-2 rounded-md bg-gray-200 w-5/6" 
                 />
-                <span className="px-2 text-gray-500">cm</span>
+                <span className="px-2 text-gray-500">{MeasurementUnit.centimetre}</span>
             </div>
             <label className="font-bold">Weight</label>
             <div>
@@ -58,7 +59,7 @@ export function ParcelInformationForm({ length, width, height, weight, updateFie
                     onChange={e => updateFields({ weight: e.target.value })}
                     className="border-2 px-2 rounded-md bg-gray-200 w-5/6" 
                 />
-                <span className="px-2 text-gray-500">kg</span>
+                <span className="px-2 text-gray-500">{MeasurementUnit.kilogram}</span>
             </div>
         </FormWrapper>
     )
--- src/components/order-form/PaymentForm.tsx
+++ src/components/order-form/PaymentForm.tsx
@@ -1,11 +1,55 @@
 import { FormWrapper } from "./FormWrapper"
 
-export function PaymentForm() {
+type PaymentData = {
+    nameOnCard: string,
+    cardNumber: string,
+    expiryDate: string,
+    securityCode: string
+}
+
+type PaymentFormProps = PaymentData & {
+    updateFields: (fields: Partial<PaymentData>) => void
+}
+
+export function PaymentForm({ nameOnCard, cardNumber, expiryDate, securityCode, updateFields }: PaymentFormProps) {
     return (
         <FormWrapper title="Payment">
-            <div>
-                
-            </div>
+            <label className="font-bold">Name on card</label>
+            <input 
+                autoFocus 
+                required 
+                type="text" 
+                value={nameOnCard} 
+                onChange={e => updateFields({ nameOnCard: e.target.value })}
+                className="border-2 px-2 rounded-md bg-gray-200" 
+            />
+            <label className="font-bold">Card number</label>
+            <input 
+                autoFocus 
+                required 
+                type="text" 
+                value={cardNumber} 
+                onChange={e => updateFields({ cardNumber: e.target.value })}
+                className="border-2 px-2 rounded-md bg-gray-200" 
+            />
+            <label className="font-bold">Expiry date</label>
+            <input 
+                autoFocus 
+                required 
+                type="month" 
+                value={expiryDate} 
+                onChange={e => updateFields({ expiryDate: e.target.value })}
+                className="border-2 px-2 rounded-md bg-gray-200" 
+            />
+            <label className="font-bold">Security code</label>
+            <input 
+                autoFocus 
+                required 
+                type="password" 
+                value={securityCode} 
+                onChange={e => updateFields({ securityCode: e.target.value })}
+                className="border-2 px-2 rounded-md bg-gray-200" 
+            />
         </FormWrapper>
     )
 }
