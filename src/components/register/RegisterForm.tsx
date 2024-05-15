161ed0831e76045ce3470e4eba0f6678b520c63c
Jonathan Ho <jonaelliho@hotmail.com>
2024-05-15 15:38:40 +0800

    register form design fix

--- src/components/register/RegisterForm.tsx
+++ src/components/register/RegisterForm.tsx
@@ -1,177 +1,216 @@
-import React, { useState, FormEvent } from 'react';
-import { AccountType } from '../../utilities/enums/AccountType';
-import { ResponseStatus } from '../../utilities/enums/ResponseStatus';
+import React, { useState, FormEvent } from "react";
+import { AccountType } from "../../utilities/enums/AccountType";
+import { ResponseStatus } from "../../utilities/enums/ResponseStatus";
 import { useNavigate } from "react-router-dom";
-import { axiosInstance } from '../security/axiosInstance';
+import { axiosInstance } from "../security/axiosInstance";
 
 export function RegisterForm() {
-    const [accountType, setAccountType] = useState(AccountType.Sender);
-    const [fullName, setFullName] = useState("");
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [phoneNo, setPhoneNo] = useState("");
-    const [vehicleCapacity, setVehicleCapacity] = useState("");
+  const [accountType, setAccountType] = useState(AccountType.Sender);
+  const [fullName, setFullName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNo, setPhoneNo] = useState("");
+  const [vehicleCapacity, setVehicleCapacity] = useState("");
 
-    const [isSenderView, setIsSenderView] = useState(true);
-    const [error, setError] = useState("");
+  const [isSenderView, setIsSenderView] = useState(true);
+  const [error, setError] = useState("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleAccountTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setIsSenderView(event.target.value === AccountType.Sender);
-        setAccountType(event.target.value === AccountType.Courier ? AccountType.Courier : AccountType.Sender);
+  const handleAccountTypeChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setIsSenderView(event.target.value === AccountType.Sender);
+    setAccountType(
+      event.target.value === AccountType.Courier
+        ? AccountType.Courier
+        : AccountType.Sender
+    );
+  };
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    const senderRegistrationInformation = {
+      fullName,
+      username,
+      email,
+      password,
+      phoneNo,
     };
-
-    function handleSubmit(e: FormEvent) {
-        e.preventDefault();
-        const senderRegistrationInformation = { fullName, username, email, password, phoneNo };
-        const courierRegistrationInformation = { fullName, username, password, vehicleCapacity };
-        const registrationInformation = accountType === AccountType.Sender ? senderRegistrationInformation : courierRegistrationInformation;
-        const url = "http://localhost:8081/register" + (accountType === AccountType.Sender ? "" : "Courier");
-        const config = {
-            headers: {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "http://localhost:3000"
-            }
+    const courierRegistrationInformation = {
+      fullName,
+      username,
+      password,
+      vehicleCapacity,
+    };
+    const registrationInformation =
+      accountType === AccountType.Sender
+        ? senderRegistrationInformation
+        : courierRegistrationInformation;
+    const url =
+      "http://localhost:8081/register" +
+      (accountType === AccountType.Sender ? "" : "Courier");
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "http://localhost:3000",
+      },
+    };
+    axiosInstance
+      .post(url, JSON.stringify(registrationInformation), config)
+      .then((response) => {
+        if (response.data.status === ResponseStatus.Success) {
+          navigate("/login", {
+            state: {
+              prepopulatedUsername: username,
+              prepopulatedPassword: password,
+            },
+          });
+        } else if (response.data.status === ResponseStatus.Failure) {
+          setError(response.data.message);
         }
-        axiosInstance.post(url, JSON.stringify(registrationInformation), config)
-            .then(response => {
-                if (response.data.status === ResponseStatus.Success) {
-                    navigate("/login", { state: { "prepopulatedUsername": username, "prepopulatedPassword": password } });
-                } else if (response.data.status === ResponseStatus.Failure) {
-                    setError(response.data.message);
-                }
-            })
-            .catch(error => {
-                alert(`Error: ${error.message}`);
-            });
-    }
+      })
+      .catch((error) => {
+        alert(`Error: ${error.message}`);
+      });
+  }
 
-    return (
-        <div className="bg-white px-10 py-20 rounded-3xl border-2 border-gray-200">
-            <h1 className="text-5xl font-semibold">FDMx</h1>
-            <p className="font-medium text-lg text-gray-500 mt-4">Please enter your registration details.</p>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label className="text-base font-normal" >Account Type:</label>
-                </div>
-                <select 
-                    id="accounttype" 
-                    name="accounttype" 
-                    className="border border-black rounded-sm p-1 mt-1 bg-transparent" 
-                    required 
-                    value={accountType}
-                    onChange={handleAccountTypeChange}
-                >
-                    <option value={AccountType.Sender}>Sender</option>
-                    <option value={AccountType.Courier}>Courier</option>
-                </select>
-                <div>
-                    <label className="text-base font-normal">Full Name:</label>
-                </div>
-                <input 
-                    type="text"
-                    value={fullName}
-                    style={{border: '1px solid black', padding: '5px'}} 
-                    id="full-name" 
-                    name="full-name" 
-                    placeholder="Full Name" 
-                    required 
-                    onChange={e => setFullName(e.target.value)}
-                >
-                </input>
-                <div>
-                    <label className="text-base font-normal">Username:</label>
-                </div>
-                <input 
-                    type="text"
-                    value={username}
-                    style={{border: '1px solid black', padding: '5px'}} 
-                    id="username" 
-                    name="username" 
-                    placeholder="Userame" 
-                    required 
-                    onChange={e => setUsername(e.target.value)}
-                >
-                </input>
-                { isSenderView && (
-                    <>
-                        <div>
-                            <label className="text-base font-normal">Email:</label>
-                        </div>
-                        <input
-                            type="email" 
-                            value={email}
-                            style={{ border: '1px solid black', padding: '5px' }}
-                            id="useremail" 
-                            name="useremail" 
-                            placeholder="Email" 
-                            required
-                            onChange={e => setEmail(e.target.value)}
-                        ></input>
-                    </>
-                )}
-                <div>
-                    <label className="text-base font-normal">Password:</label>
-                </div>
-                <input 
-                    type="password" 
-                    value={password}
-                    style={{ border: '1px solid black', padding: '5px' }}
-                    id="password" 
-                    name="password" 
-                    placeholder="Password" 
-                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                    title="Must contain minimum 8 characters, at least 1 number, 1 uppercase and 1 lowercase letter" 
-                    required
-                    onChange={e => setPassword(e.target.value)}
-                ></input>
-                { isSenderView && (
-                    <>
-                        <div>
-                            <label className="text-base font-normal">Phone Number:</label>
-                        </div>
+  return (
+    <div className="bg-white px-10 py-12 rounded-3xl border-2 border-gray-200">
+      <h1 className="text-5xl font-semibold">FDMx</h1>
+      <p className="font-medium text-lg text-gray-500 mt-4">
+        Please enter your registration details.
+      </p>
+      <div className="mt-6">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label className="text-lg font-medium">Account Type:</label>
+          </div>
+          <select
+            id="accounttype"
+            name="accounttype"
+            className="w-full border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent"
+            required
+            value={accountType}
+            onChange={handleAccountTypeChange}
+          >
+            <option value={AccountType.Sender}>Sender</option>
+            <option value={AccountType.Courier}>Courier</option>
+          </select>
+          {/* <label className="text-lg font-medium">Username</label>
                         <input 
-                            type="tel" 
-                            value={phoneNo}
-                            style={{ border: '1px solid black', padding: '5px' }}
-                            id="phone-number" 
-                            name="phone-number" 
-                            placeholder="Phone Number" 
-                            required
-                            onChange={e => setPhoneNo(e.target.value)}
-                        ></input>
-                    </>
-                )}
-                { !isSenderView && (
-                    <>
-                        <div> 
-                            <label className="text-base font-normal">Vehicle Capacity (kg):</label> 
-                        </div>
-                        <input 
-                            type="text" 
-                            value={vehicleCapacity}
-                            style={{border: '1px solid black', padding: '5px'}} 
-                            id="capacity" 
-                            name="capacity" 
-                            placeholder="0"
-                            onChange={e => setVehicleCapacity(e.target.value)}
-                        ></input>
-                    </>
-                )}
-                { error && (
-                    <p className="text-red-500">{error}</p>
-                )}
-                <div className="mt-8 flex flex-col gap-y-4">
-                    <button 
-                        type="submit"
-                        className="active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-slate-500 text-white text-lg font-bold"
-                    >
-                        Register
-                    </button>
-                </div>
-            </form>
-        </div>
-    )
+                            className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+                            placeholder="Enter your username"
+                            type="text"
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
+                            required 
+                        /> */}
+          <div>
+            <label className="text-lg font-medium">Full Name:</label>
+          </div>
+          <input
+            type="text"
+            value={fullName}
+            className="w-full border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent"
+            id="full-name"
+            name="full-name"
+            placeholder="Full Name"
+            required
+            onChange={(e) => setFullName(e.target.value)}
+          ></input>
+          <div>
+            <label className="text-lg font-medium">Username:</label>
+          </div>
+          <input
+            type="text"
+            value={username}
+            className="w-full border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent"
+            id="username"
+            name="username"
+            placeholder="Userame"
+            required
+            onChange={(e) => setUsername(e.target.value)}
+          ></input>
+          {isSenderView && (
+            <>
+              <div>
+                <label className="text-lg font-medium">Email:</label>
+              </div>
+              <input
+                type="email"
+                value={email}
+                className="w-full border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent"
+                id="useremail"
+                name="useremail"
+                placeholder="Email"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+            </>
+          )}
+          <div>
+            <label className="text-lg font-medium">Password:</label>
+          </div>
+          <input
+            type="password"
+            value={password}
+            className="w-full border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent"
+            id="password"
+            name="password"
+            placeholder="Password"
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            title="Must contain minimum 8 characters, at least 1 number, 1 uppercase and 1 lowercase letter"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          {isSenderView && (
+            <>
+              <div>
+                <label className="text-lg font-medium">Phone Number:</label>
+              </div>
+              <input
+                type="tel"
+                value={phoneNo}
+                className="w-full border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent"
+                id="phone-number"
+                name="phone-number"
+                placeholder="Phone Number"
+                required
+                onChange={(e) => setPhoneNo(e.target.value)}
+              ></input>
+            </>
+          )}
+          {!isSenderView && (
+            <>
+              <div>
+                <label className="text-lg font-medium">
+                  Vehicle Capacity (kg):
+                </label>
+              </div>
+              <input
+                type="text"
+                value={vehicleCapacity}
+                className="w-full border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent"
+                id="capacity"
+                name="capacity"
+                placeholder="0"
+                onChange={(e) => setVehicleCapacity(e.target.value)}
+              ></input>
+            </>
+          )}
+          {error && <p className="text-red-500">{error}</p>}
+          <div className="mt-8 flex flex-col gap-y-4">
+            <button
+              type="submit"
+              className="active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-slate-500 text-white text-lg font-bold"
+            >
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,9 +1,17 @@
 import { RegisterForm } from "../components/register/RegisterForm";
 
 export function RegisterPage() {
-    return (
-        <>
-            <RegisterForm />
-        </>
-    )
+  return (
+    <>
+      <div className="flex w-full h-screen">
+        <div className="w-full flex items-center justify-center lg:w-1/2">
+          <RegisterForm />
+        </div>
+        <div className="hidden relative lg:flex h-full w-1/2 items-center justify-center bg-gray-200">
+          <div className="w-60 h-60 bg-gradient-to-tr from-slate-500 to-slate-400 rounded-full animate-spin"></div>
+          <div className="w-full h-1/2 absolute bottom-0 bg-white/10 backdrop-blur-lg"></div>
+        </div>
+      </div>
+    </>
+  );
 }
