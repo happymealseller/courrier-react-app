5821802685459841eeebc3870cce9977eb6b3196
Jonathan Ho <jonaelliho@hotmail.com>
2024-05-20 18:03:46 +0800

    courier dashboard html fixes

--- src/components/dashboard/CourierDashboard.tsx
+++ src/components/dashboard/CourierDashboard.tsx
@@ -1,65 +1,69 @@
-import React, { useState, FormEvent, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { LocalStorageData } from '../../App';
+import { FormEvent, useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { LocalStorageData } from "../../App";
+import { axiosInstance } from "../security/axiosInstance";
+import { format } from "date-fns";
 
 export type CourierDashboardProps = {
-    sendDataToApp: (data: LocalStorageData) => void
-}
+  sendDataToApp: (data: LocalStorageData) => void;
+};
 
-export function CourierDashboard({sendDataToApp}: CourierDashboardProps) {
-  const [courierId, setCourierId] = useState("");
-  const [orderId, setOrderId] = useState("");
+const filterData = (data: any[], keys: any[]) => {
+  return data.map((item) => {
+    const filteredItem: any = {}; // Add type annotation here
+    keys.forEach((key) => {
+      filteredItem[key] = key.includes("Date")
+        ? format(new Date(item[key]), "yyyy-MM-dd")
+        : item[key];
+    });
+    return filteredItem as OrderHistoryItem;
+  });
+};
+
+export function CourierDashboard({ sendDataToApp }: CourierDashboardProps) {
+  const [orders, setOrders] = useState<OrderHistoryItem[]>([]);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { jwt, accountType, username } = location.state || {};
+  useEffect(() => {
+    sendDataToApp({ jwt, accountType, username });
+  }, []);
+
   const order_headers = [
     "order_id",
     "sender_name",
     "receipient_name",
     "delivery_address",
-    "status",
     "delivery_date",
+    "status",
   ];
 
-  const people = [
-    {
-      order_id: "1",
-      sender_name: "Creola Katherine Johnson",
-      receipient_name: "Creola Johnson",
-      delivery_address: "22 Jump Street",
-      delivery_date: "2022-01-01",
-      status: "Delivered",
-    },
-    {
-      order_id: "2",
-      sender_name: "Mario José Molina-Pasquel Henríquez",
-      receipient_name: "Creola Johnson",
-      delivery_address: "2 Suntec Street",
-      delivery_date: "2023-01-01",
-      status: "Pending",
-    },
-    {
-      order_id: "3",
-      sender_name: "Mohammad Abdus Salam",
-      receipient_name: "Creola Johnson",
-      delivery_address: "11 Suntec Street",
-      delivery_date: "2024-01-01",
-      status: "Returned",
-    },
-    {
-      order_id: "4",
-      sender_name: "Mohammad Abdus Salam",
-      receipient_name: "Percy Lavon Julian",
-      delivery_address: "11 Suntec Street",
-      delivery_date: "2024-02-01",
-      status: "Assigned",
-    },
-  ]; // to update with actual data from db, filter out for only undelivered items
+  const displayKeys = [
+    "orderId",
+    "fromFullName",
+    "toFullName",
+    "toAddress",
+    "deliveryDate",
+    "orderStatus",
+  ];
 
-    const navigate = useNavigate();
-    const location = useLocation();
-    const { jwt, accountType, username } = location.state || {};
-    useEffect(() => {
-        sendDataToApp({ jwt, accountType, username });
-    }, []);
-    
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "http://localhost:3000",
+      jwt: localStorage.getItem("jwt"),
+      username: localStorage.getItem("username"),
+    },
+  };
+
+  useEffect(() => {
+    axiosInstance
+      .get("/courier/orders", config) //  url to change also
+      .then((res) => filterData(res.data.orderHistoryList, displayKeys))
+      .then((data: OrderHistoryItem[]) => setOrders(data))
+      .catch((err) => console.log(err));
+  }, [setOrders]); // to filter only for undelivered items next time
 
   //overall function need to update
   // function handleSubmit(e: FormEvent) {
@@ -98,57 +102,60 @@ export function CourierDashboard({sendDataToApp}: CourierDashboardProps) {
         Welcome User
       </h2>
       <br></br>
-      <form onClick={handleClick}>
-        <table className="table-auto w-full">
-          <thead>
-            <tr className="border border-black px-5 py-2">
-              {order_headers.map((header) => (
-                <th key={header} className="border border-black px-5 py-2">
-                  {header}
-                </th>
-              ))}
-              <th className="border border-black px-5 py-2">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {people.map((item, index) => (
-              <tr key={index} className="bg-white">
-                {Object.entries(item).map(([key, value], idx) => (
-                  <td
-                    key={idx}
-                    className="border border-black px-5 py-2 border-solid"
-                    style={{ textAlign: "center" }}
-                  >
-                    {value}
-                  </td>
-                ))}
-                {/* Button in the last column, to fix issue with last column header borders next time */}
-                <td 
-                  className="border px-5 py-2 border-black"
+      <table className="table-auto w-full">
+        <thead>
+          <tr className="border border-black px-5 py-2">
+            {order_headers.map((header, idx) => (
+              <th key={idx} className="border border-black px-5 py-2">
+                {header}
+              </th>
+            ))}
+            <th className="border border-black px-5 py-2">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((item, index) => (
+            <tr
+              key={index}
+              className="bg-white border border-black px-5 py-2 border-solid"
+            >
+              {Object.entries(item).map(([key, value], idx) => (
+                <td
+                  key={idx}
+                  className="border border-black px-5 py-2 border-solid"
                   style={{ textAlign: "center" }}
                 >
-                  <div style={{ padding: "10px 15px", alignItems: "center" }}>
-                    <button
-                      type="button"
-                      className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded"
-                    >
-                      View
-                    </button>
-                  </div>
+                  {value}
                 </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <div className="mt-8 gap-y-6">
-          <button
-            type="button"
-            className="active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-slate-500 text-white text-lg font-bold px-8 py-3 text-center m-2 "
-          >
-            Back
-          </button>
-        </div>
-      </form>
+              ))}
+              {/* Button in the last column, to fix issue with last column header borders next time */}
+              <td
+                className="border px-5 py-2 border-black"
+                style={{ textAlign: "center" }}
+              >
+                <div style={{ padding: "10px 15px", alignItems: "center" }}>
+                  <button
+                    type="button"
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded"
+                    onClick={handleClick}
+                  >
+                    View
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="mt-8 gap-y-6">
+        <button
+          type="button"
+          className="active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-slate-500 text-white text-lg font-bold px-8 py-3 text-center m-2 "
+          onClick={handleClick}
+        >
+          Back
+        </button>
+      </div>
     </div>
   );
 }
