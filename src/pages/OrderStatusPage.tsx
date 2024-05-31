f7187c5127eea0d295ddc9af5fe5744fbb33c0d7
Lim Yang Yu <yangyulim101@gmail.com>
2024-05-31 10:24:27 +0800

    Modify/add order details

--- src/pages/OrderStatusPage.tsx
+++ src/pages/OrderStatusPage.tsx
@@ -37,7 +37,7 @@ const sampleStatuses = JSON.parse(`[
     {"status": "DELIVERING", "remarks": "Delivering order", "statusUpdateDate": "2024-05-14T09:44:22.769+00:00"},
     {"status": "DELIVERED", "remarks": "Delivered order", "statusUpdateDate": "2024-05-15T10:40:22.769+00:00"},
     {"status": "CANCELLED", "remarks": "Cancelled order", "statusUpdateDate": "2024-05-16T11:10:22.769+00:00"},
-    {"status": "OTHER", "remarks": "Some kind of remark for other category", "statusUpdateDate": "2024-05-17T04:12:22.769+00:00"}
+    {"status": "OTHER", "remarks": "Some kind of remark for other category Some kind of remark for other category Some kind of remark for other category Some kind of remark for other category", "statusUpdateDate": "2024-05-17T04:12:22.769+00:00"}
 ]`);
 sampleStatuses.reverse();
 
@@ -53,102 +53,65 @@ function OrderStatusPage() {
     console.log("[REDIRECT - from TrackSearchBarPage] Response data: ", state);
     const statusesArray = state.orderStatus.reverse();
 
-    //delivery date
-    //order date
-
-    //from address
-    //from fullname
-    //from email
-    //from phoneNo
-
-    //to address
-    //to fullname
-    //to email
-    //to phoneNo
-
-    //parcel info
-    //W x H x L
-    //weight
-    //parcel descp
-
     return (
         <>
             <section className="bg-white py-8 px-10 antialiased dark:bg-gray-900 md:py-16">
-                <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
+                <div className="mx-auto max-w-min px-4 2xl:px-0">
                     <h2 className="text-xl font-bold leading-none text-gray-900 dark:text-white sm:text-2xl">Track the delivery of order #{state.orderId}</h2>
 
                     <div className="mt-6 sm:mt-8 lg:flex lg:gap-8">
                         <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
                             <div className="flex items-center justify-between mb-4">
                                 <h3 className="text-xl font-semibold text-gray-900 dark:text-white">Order Details</h3>
-
                             </div>
                             <div className="flow-root">
                                 <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
                                     <li className="py-3 sm:py-4" key={1}>
-                                        <div className="flex items-center">
-                                            <div className="flex-1 min-w-0 ms-4">
-                                                <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                                    {`Expected Arrival Date: ${formatTimestamp(state.deliveryDate, false)}`}
-                                                </p>
-                                                <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                                                    {`Order date: ${formatTimestamp(state.orderDate, false)}`}
-                                                </p>
-                                            </div>
+                                        <div className="flex-1 min-w-0 ms-4">
+                                            <p className="text-base font-semibold text-gray-900 truncate dark:text-white">
+                                                {`Expected Arrival Date: ${formatTimestamp(state.deliveryDate, false)}`}
+                                            </p>
+                                            <p className="text-sm font-medium text-gray-500 truncate dark:text-gray-400">
+                                                {`Order date: ${formatTimestamp(state.orderDate, false)}`}
+                                            </p>
                                         </div>
                                     </li>
                                     <li className="py-3 sm:py-4" key={2}>
-                                        <div className="flex items-center ">
-                                            <div className="flex-1 min-w-0 ms-4">
-                                                <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                                    {`From:`}
-                                                </p>
-                                                <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                                                    {``}
-                                                </p>
-                                            </div>
+                                        <div className="flex-1 min-w-0 ms-4">
+                                            <p className="text-base font-semibold text-gray-900 truncate dark:text-white">
+                                                {`From:`}
+                                            </p>
+                                            <p className="text-sm font-medium whitespace-pre-line text-gray-500 truncate dark:text-gray-400">
+                                                {`Address: ${state.fromAddress}\n`}
+                                                {`Name: ${state.fromFullName}\n`}
+                                                {`Email: ${state.fromEmail}\n`}
+                                                {`Phone: ${state.fromPhoneNo}\n`}
+                                            </p>
                                         </div>
                                     </li>
                                     <li className="py-3 sm:py-4" key={3}>
-                                        <div className="flex items-center">
-                                            <div className="flex-1 min-w-0 ms-4">
-                                                <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                                    Michael Gough
-                                                </p>
-                                                <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                                                    email@windster.com
-                                                </p>
-                                            </div>
+                                        <div className="flex-1 min-w-0 ms-4">
+                                            <p className="text-base font-semibold text-gray-900 truncate dark:text-white">
+                                                {`To:`}
+                                            </p>
+                                            <p className="text-sm font-medium whitespace-pre-line text-gray-500 truncate dark:text-gray-400">
+                                                {`Address: ${state.toAddress}\n`}
+                                                {`Name: ${state.toFullName}\n`}
+                                                {`Email: ${state.toEmail}\n`}
+                                                {`Phone: ${state.toPhoneNo}\n`}
+                                            </p>
                                         </div>
                                     </li>
                                     <li className="py-3 sm:py-4" key={4}>
-                                        <div className="flex items-center ">
-                                            <div className="flex-shrink-0">
-                                                <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-4.jpg" alt="Lana image" />
-                                            </div>
-                                            <div className="flex-1 min-w-0 ms-4">
-                                                <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                                    Lana Byrd
-                                                </p>
-                                                <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                                                    email@windster.com
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li className="pt-3 pb-0 sm:pt-4" key={5}>
-                                        <div className="flex items-center ">
-                                            <div className="flex-shrink-0">
-                                                <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="Thomas image" />
-                                            </div>
-                                            <div className="flex-1 min-w-0 ms-4">
-                                                <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                                    Thomes Lean
-                                                </p>
-                                                <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                                                    email@windster.com
-                                                </p>
-                                            </div>
+                                        <div className="flex-1 min-w-0 ms-4">
+                                            <p className="text-base font-semibold text-gray-900 truncate dark:text-white">
+                                                Parcel Information:
+                                            </p>
+                                            <p className="text-sm font-medium whitespace-pre-line text-gray-500 truncate dark:text-gray-400">
+                                                {`Dimensions (W x L x H): ${state.width}cm x ${state.length}cm x ${state.height}cm \n`}
+                                                {`Weight (KG): ${state.weight}\n`}
+                                                {`Description: ${state.parcelDescription}\n`}
+                                            </p>
                                         </div>
                                     </li>
                                 </ul>
@@ -160,11 +123,10 @@ function OrderStatusPage() {
                             <div className="space-y-6 rounded-lg border pb-0 border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
                                 <h3 className="text-xl font-semibold text-gray-900 dark:text-white">Order history</h3>
 
-                                <ol className="relative ms-3 border-s border-gray-200 dark:border-gray-700">
-
-                                    // have to change sampleStatuses to statusesArray for the .map method and comparison of length
-                                    {sampleStatuses.map((element: statusProperty, index: number) => {
-                                        const isMostRecentTask = (sampleStatuses.length === 1) ? true : (index === 0);
+                                <ol className="relative ms-3 border-s min-w-72 max-w-min border-gray-200 dark:border-gray-700">
+                                    
+                                    {statusesArray.map((element: statusProperty, index: number) => {
+                                        const isMostRecentTask = (statusesArray.length === 1) ? true : (index === 0);
                                         const listItemClass: string = isMostRecentTask ? "text-gray-900" : "text-gray-400";
                                         const formattedDatetime = formatTimestamp(element.statusUpdateDate, true);
 
@@ -181,7 +143,6 @@ function OrderStatusPage() {
                                             </li>
                                         </>);
                                     })}
-
                                 </ol>
                             </div>
                         </div>
