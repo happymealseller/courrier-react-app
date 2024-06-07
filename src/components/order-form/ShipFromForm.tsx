698733ab7f00f67509a88cfcc3fd4fde045c7ea4
Jonathan Ho <jonaelliho@hotmail.com>
2024-06-07 10:09:15 +0800

    revert back address fields correctly

--- src/components/order-form/ShipFromForm.tsx
+++ src/components/order-form/ShipFromForm.tsx
@@ -28,17 +28,6 @@ export function ShipFromForm({ fromAddress, fromFullName, fromEmail, fromPhone, 
 
     return (
         <FormWrapper title="Ship From (Sender)">
-            {/*
-            <label className="font-bold">Full Name or Company Name</label>
-            <input 
-                autoFocus 
-                required 
-                type="text" 
-                value={fromCompanyName} 
-                onChange={e => updateFields({ fromCompanyName: e.target.value })}
-                className="border-2 px-2 rounded-md bg-gray-200" 
-            />
-            */}
             <label className="font-bold">Full Name</label>
             <input 
                 autoFocus 
@@ -50,11 +39,45 @@ export function ShipFromForm({ fromAddress, fromFullName, fromEmail, fromPhone, 
             />
             <label className="font-bold">Address</label>
             <input 
-                autoFocus 
-                required 
+                autoFocus
+                type="text"
+                value={address}
+                onChange={e => setAddress(e.target.value)}
+                className="border-2 px-2 rounded-md bg-gray-200"
+            />
+                <label className="font-bold">Postal Code</label>
+            <div>
+                <input 
+                    autoFocus
+                    type="text" 
+                    pattern="^\d{0,6}$"
+                    title="Please enter 6 digits"
+                    value={fromAddress.postalCode}
+                    onChange={e => updateFields({ fromAddress: { ...fromAddress, postalCode: e.target.value }})}
+                    className="border-2 px-2 rounded-md bg-gray-200"
+                />
+                <button
+                    type="button"
+                    className="bg-slate-500 rounded-md w-1/2 text-sm text-white ml-1 p-1"
+                    onClick={(e) => handleAddressRetrieval(e, fromAddress.postalCode || "")}
+                >
+                    Retrieve address
+                </button>
+            </div>
+            <label className="font-bold">City</label>
+            <input 
+                autoFocus
                 type="text" 
-                value={fromAddress}
-                onChange={e => updateFields({ fromAddress: e.target.value })}
+                value={city}
+                onChange={e => setCity(e.target.value)}
+                className="border-2 px-2 rounded-md bg-gray-200"
+            />
+            <label className="font-bold">Country</label>
+            <input 
+                autoFocus
+                type="text" 
+                value={country}
+                onChange={e => setCountry(e.target.value)}
                 className="border-2 px-2 rounded-md bg-gray-200"
             />
             <label className="font-bold">Email</label>
--- src/components/order-form/ShipToForm.tsx
+++ src/components/order-form/ShipToForm.tsx
@@ -24,17 +24,6 @@ export function ShipToForm({ toAddress, toFullName, toEmail, toPhone, updateFiel
 
     return (
         <FormWrapper title="Ship To (Receipient)">
-            {/*
-            <label className="font-bold">Full Name or Company Name</label>
-            <input 
-                autoFocus 
-                required 
-                type="text" 
-                value={toCompanyName} 
-                onChange={e => updateFields({ toCompanyName: e.target.value })} 
-                className="border-2 px-2 rounded-md bg-gray-200"
-            />
-            */}
             <label className="font-bold">Contact Name</label>
             <input 
                 autoFocus 
@@ -49,8 +38,46 @@ export function ShipToForm({ toAddress, toFullName, toEmail, toPhone, updateFiel
                 autoFocus 
                 required 
                 type="text" 
-                value={toAddress}
-                onChange={e => updateFields({ toAddress: e.target.value })} 
+                value={address}
+                onChange={e => setAddress(e.target.value)}
+                className="border-2 px-2 rounded-md bg-gray-200"
+            />
+            <label className="font-bold">Postal Code</label>
+            <div>
+                <input 
+                    autoFocus 
+                    required 
+                    type="text" 
+                    pattern="^\d{0,6}$"
+                    title="Please enter 6 digits"
+                    value={toAddress.postalCode}
+                    onChange={e => updateFields({ toAddress: { ...toAddress, postalCode: e.target.value }})}
+                    className="border-2 px-2 rounded-md bg-gray-200"
+                />
+                <button
+                    type="button"
+                    className="bg-slate-500 rounded-md w-1/2 text-sm text-white ml-1 p-1"
+                    onClick={(e) => handleAddressRetrieval(e, toAddress.postalCode || "")}
+                >
+                    Retrieve address
+                </button>
+            </div>
+            <label className="font-bold">City</label>
+            <input 
+                autoFocus 
+                required 
+                type="text" 
+                value={city}
+                onChange={e => setCity(e.target.value)}
+                className="border-2 px-2 rounded-md bg-gray-200"
+            />
+            <label className="font-bold">Country</label>
+            <input 
+                autoFocus 
+                required 
+                type="text" 
+                value={country}
+                onChange={e => setCountry(e.target.value)}
                 className="border-2 px-2 rounded-md bg-gray-200"
             />
             <label className="font-bold">Recipient Email</label>
