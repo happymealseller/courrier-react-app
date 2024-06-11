3adeaa68a0905d71f98e64f853d0189212a16851
happymeal <happymeal>
2024-06-11 15:22:46 +0800

    fix: extra map

--- src/components/dashboard/CourierDashboard.tsx
+++ src/components/dashboard/CourierDashboard.tsx
@@ -46,43 +46,42 @@ export function CourierDashboard() {
 
   return (
     <div className="p-20">
-      {orders.map((e) => {
-        return <table className="min-w-full divide-y divide-gray-200">
-          <thead className="bg-gray-50">
-            <tr>
-              <th className="px-6 py-4 text-center text-md text-gray-500 uppercase tracking-wider">ID</th>
-              <th className="px-6 py-4 text-center text-md text-gray-500 uppercase tracking-wider">Date</th>
-              <th className="px-6 py-4 text-center text-md text-gray-500 uppercase tracking-wider">From</th>
-              <th className="px-6 py-4 text-center text-md text-gray-500 uppercase tracking-wider">To</th>
-              <th className="px-6 py-4 text-center text-md text-gray-500 uppercase tracking-wider">Status</th>
-              <th className="px-6 py-4 text-center text-md text-gray-500 uppercase tracking-wider"></th>
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+          <tr>
+            <th className="px-6 py-4 text-center text-md text-gray-500 uppercase tracking-wider">ID</th>
+            <th className="px-6 py-4 text-center text-md text-gray-500 uppercase tracking-wider">Date</th>
+            <th className="px-6 py-4 text-center text-md text-gray-500 uppercase tracking-wider">From</th>
+            <th className="px-6 py-4 text-center text-md text-gray-500 uppercase tracking-wider">To</th>
+            <th className="px-6 py-4 text-center text-md text-gray-500 uppercase tracking-wider">Status</th>
+            <th className="px-6 py-4 text-center text-md text-gray-500 uppercase tracking-wider"></th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {orders.map((e) => (
+            <tr key={e.id}>
+              <td className="px-6 py-4 text-center text-sm whitespace-nowrap">{e.id}</td>
+              <td className="px-6 py-4 text-center text-sm whitespace-nowrap">{e.date}</td>
+              <td className="px-6 py-4 text-center text-sm whitespace-nowrap">{e.from}</td>
+              <td className="px-6 py-4 text-center text-sm whitespace-nowrap">{e.to}</td>
+              <td className="px-6 py-4 text-center text-sm whitespace-nowrap">{e.status}</td>
+
+              <td className="px-6 py-4 text-center text-sm whitespace-nowrap">
+                <select onChange={handleChange} className="border border-gray-300 rounded px-3 py-1 text-gray-700">
+                  <option value="RETRIEVED">Retrieve</option>
+                  <option value="COMPLETED">Complete</option>
+                </select>
+                <button
+                  onClick={() => handleUpdate(e.id)}
+                  className="ml-2 px-2 py-1 bg-blue-500 text-white rounded hover:bg-blue-700">
+                  Update
+                </button>
+
+              </td>
             </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {orders.map((e) => (
-              <tr key={e.id}>
-                <td className="px-6 py-4 text-center text-sm whitespace-nowrap">{e.id}</td>
-                <td className="px-6 py-4 text-center text-sm whitespace-nowrap">{e.date}</td>
-                <td className="px-6 py-4 text-center text-sm whitespace-nowrap">{e.from}</td>
-                <td className="px-6 py-4 text-center text-sm whitespace-nowrap">{e.to}</td>
-                <td className="px-6 py-4 text-center text-sm whitespace-nowrap">{e.status}</td>
-                <td className="px-6 py-4 text-center text-sm whitespace-nowrap">
-                  <td className="px-6 py-4 text-center text-sm whitespace-nowrap">
-                    <select onChange={handleChange} className="border border-gray-300 rounded px-3 py-1 text-gray-700">
-                      <option value="RETRIEVED">Retrieve</option>
-                      <option value="COMPLETED">Complete</option>
-                    </select>
-                    <button
-                      onClick={() => handleUpdate(e.id)}
-                      className="ml-2 px-2 py-1 bg-blue-500 text-white rounded hover:bg-blue-700">
-                      Update
-                    </button>
-                  </td>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+          ))}
+        </tbody>
+      </table>
       })}
     </div>
   )
